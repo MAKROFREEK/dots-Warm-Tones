2dcd71e9d692b7729c1692ad2b525cf5b258c55b
christian jones <84992148+LetsHaveKiddos@users.noreply.github.com>
2021-08-12 13:50:17 -0500

    I can't believe I forgot rofi

---
+++ home/.config/rofi/scripts/i3lock.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+## Get colors -----------------
+FG="#f2f2f4"
+BG="#2e3440"
+
+BLACK="#15131A"
+RED="#fa747b"
+GREEN="#72a0a6"
+YELLOW="#f6bbb8"
+BLUE="#7f99a6"
+MAGENTA="#dc554f"
+CYAN="#8cb6cc"
+WHITE="#d3d7db"
+
+TOTD=`fortune -n 90 -s | head -n 1`
+
+## Exec	-----------------
+i3lock \
+--color="${BG}D9"			\
+\
+--insidever-color=${GREEN}	\
+--insidewrong-color=${RED}	\
+--inside-color="${BG}00"	\
+\
+--ringver-color=${GREEN}	\
+--ringwrong-color=${RED}	\
+--ring-color=${BLUE}		\
+\
+--line-color=${BG}			\
+--separator-color=${BG}		\
+\
+--keyhl-color=${YELLOW}		\
+--bshl-color=${RED}			\
+\
+--verif-color=${BG}			\
+--wrong-color=${FG}			\
+--layout-color=${FG}		\
+\
+--time-color=${FG}			\
+--date-color=${FG}			\
+\
+--pass-media-keys			\
+--pass-screen-keys			\
+--pass-power-keys			\
+--pass-volume-keys			\
+--{time,date,layout,verif,wrong,greeter}-font="DejaVu Sans"			\
+--{layout,verif,wrong,greeter}-size=18							\
+--time-size=34													\
+--date-size=18													\
+--greeter-text="think outside the box"			\
+--greeter-color=${WHITE}				\
+--verif-text="verifying..."			\
+--wrong-text="wrong!"				\
+--noinput-text="Empty"				\
+--lock-text="Locking..."			\
+--lockfailed-text="Failed to lock"	\
+--radius 120				\
+--ring-width 8.0			\
+--screen 1					\
+--clock						\
+--indicator					\
+--time-str="%I:%M %p"		\
+--date-str="%b %d, %G"		\
+#--blur 5					\
